c81bd3465da2be8529477df1d963c60f17c1fc33
kris1729 <surajchauhan8545@gmail.com>
2025-05-28 12:39:06 +0530

    complete funnction

--- send a Request form the server/log_of_all_req.js
+++ send a Request form the server/log_of_all_req.js
@@ -1,13 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
-const myServer = http.createServer((req,res)=>{
+
+const myServer = http.createServer((req, res) => {
     console.log('new Req is find');
-    res.end('hay! i am form server side')
+
+    // no blocking 
+    const loge = `${Date.now().toLocaleString()} : ${req.url} --> new request`;
+    fs.appendFile("loge.txt",loge , (err, data) => {
+        res.end('hay! i am form server side');
+    })
+
+
 })
 
 
 // lestion this server
 
-myServer.listen(4400,()=>{
+myServer.listen(4400, () => {
     console.log("server is started at the port 4400")
 })
